166ba8ba641d510400ad4c335eebfe51acceb647
Etienne Beyeler <etiennebeyeler@gmail.com>
2018-12-06 16:58:01 -0800

    fix stuff in UI

--- AutoReservation.UI/AutoView.xaml.cs
+++ AutoReservation.UI/AutoView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AutoReservation.UI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AutoView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AutoView : Window
     {
-        public MainWindow()
+        public AutoView()
         {
             InitializeComponent();
         }
