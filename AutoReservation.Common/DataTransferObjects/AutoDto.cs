69d8d074ffdd456829a9dad0b16885f42dc4b0d9
Dani <daniel.bucher@windowslive.com>
2018-12-07 08:41:40 +0100

    added dto classes

--- AutoReservation.Common/DataTransferObjects/AutoDto.cs
+++ AutoReservation.Common/DataTransferObjects/AutoDto.cs
@@ -2,10 +2,14 @@
 {
     public class AutoDto
     {
+        public int Id { get; set; }
+        public string Marke { get; set; }
+        public int Tagestarif { get; set; }
+        public byte? RowVersion { get; set; }
+        public AutoKlasse AutoKlasse { get; set; }
+        public int? Basistarif { get; set; }
 
-
-
-        //public override string ToString()
-        //    => $"{Id}; {Marke}; {Tagestarif}; {Basistarif}; {AutoKlasse}; {RowVersion}";
+        public override string ToString()
+            => $"{Id}; {Marke}; {Tagestarif}; {Basistarif}; {AutoKlasse}; {RowVersion}";
     }
 }
--- AutoReservation.Common/DataTransferObjects/KundeDto.cs
+++ AutoReservation.Common/DataTransferObjects/KundeDto.cs
@@ -1,9 +1,17 @@
-﻿namespace AutoReservation.Common.DataTransferObjects
+﻿using System;
+
+namespace AutoReservation.Common.DataTransferObjects
 {
     public class KundeDto
     {
+        public int Id { get; set; }
+        public string Nachname { get; set; }
+        public string Vorname { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+        public byte? RowVersion { get; set; }
 
-        //public override string ToString()
-        //    => $"{Id}; {Nachname}; {Vorname}; {Geburtsdatum}; {RowVersion}";
+
+        public override string ToString()
+            => $"{Id}; {Nachname}; {Vorname}; {Geburtsdatum}; {RowVersion}";
     }
 }
