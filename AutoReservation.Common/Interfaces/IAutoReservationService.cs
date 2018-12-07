5b224db7c97ef827c854a1976b91aab7c3114355
Dani <daniel.bucher@windowslive.com>
2018-12-07 09:35:32 +0100

    changes service-layer

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,6 +1,39 @@
-﻿namespace AutoReservation.Common.Interfaces
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.ServiceModel;
+using AutoReservation.Common.DataTransferObjects;
+
+namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+//        [OperationContract]
+//        bool CheckAvailability(int autoId);
+//
+//        [OperationContract]
+//        Collection<AutoDto> ReadAutoDtos(Collection<int> autoId);
+//
+//        [OperationContract]
+//        AutoDto ReadAutoDto(int autoId);
+
+//        [OperationContract]
+//        void insertAuto(string marke, int tagestarif, )
+
+        [OperationContract]
+        Collection<KundeDto> ReadKundeDtos(Collection<int> kundenId);
+
+        [OperationContract]
+        AutoDto ReadKundeDto(int kundeId);
+
+        [OperationContract]
+        void insertKunde(int id, string nachname, string vorname, DateTime geburtsDatum, byte?[] rowVersion);
+
+        [OperationContract]
+        void updateKunde(int id, string nachname, string vorname, DateTime geburtsDatum, byte?[] rowVersion);
+
+        [OperationContract]
+        void deleteKunde(int id);
     }
 }
--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Common.Interfaces;
 
 namespace AutoReservation.Service.Wcf
 {
-    public class AutoReservationService
+    public class AutoReservationService : IAutoReservationService
     {
+
+
 
         private static void WriteActualMethod()
             => Console.WriteLine($"Calling: {new StackTrace().GetFrame(1).GetMethod().Name}");
+
+
+        public Collection<KundeDto> ReadKundeDtos(Collection<int> kundenId)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public AutoDto ReadKundeDto(int kundeId)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void insertKunde(int id, string nachname, string vorname, DateTime geburtsDatum, byte?[] rowVersion)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void updateKunde(int id, string nachname, string vorname, DateTime geburtsDatum, byte?[] rowVersion)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
+
+        public void deleteKunde(int id)
+        {
+            WriteActualMethod();
+            throw new NotImplementedException();
+        }
     }
 }
