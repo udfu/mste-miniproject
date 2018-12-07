7a9639e39ceb1741508e8b42aa70421e0a3f4279
udfu <dario_delpiero10@hotmail.com>
2018-12-07 09:33:55 +0100

    first crud working

--- AutoReservation.BusinessLayer.Testing/KundeUpdateTest.cs
+++ AutoReservation.BusinessLayer.Testing/KundeUpdateTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using AutoReservation.Dal.Entities;
 using AutoReservation.TestEnvironment;
 using Xunit;
 
@@ -11,9 +13,33 @@ namespace AutoReservation.BusinessLayer.Testing
         private KundeManager Target => target ?? (target = new KundeManager());
 
         [Fact]
+        public void KundeTest()
+        {
+
+        }
+
+
+        [Fact]
         public void UpdateKundeTest()
         {
-            throw new NotImplementedException("Test not implemented.");
+            Target.UpdateKunde(1, new DateTime(1994,9,17), "Fuoco", "Dario");
+            Kunde kunde = Target.GetKundeById(1);
+            Assert.Equal("Dario", kunde.Vorname);
+        }
+
+        [Fact]
+        public void AddKundeTest()
+        {
+            Kunde newKunde = new Kunde { Id = 5, Nachname = "Bucher", Vorname = "Daniel", Geburtsdatum = new DateTime(1995, 05, 05) };
+            Target.AddKunde(newKunde);
+            Kunde dani = Target.GetKundeById(5);
+            Assert.Equal("Bucher", dani.Nachname);
+        }
+
+        [Fact]
+        public void GetKundenTest()
+        {   
+            Assert.Equal(4, Target.GetKunden().Count);
         }
     }
 }
