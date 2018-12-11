d3a9925e232e6b907c2f497e66f298f93ef4b3e0
Etienne Beyeler <etiennebeyeler@gmail.com>
2018-12-11 10:40:11 -0800

    add basic view model for cars

---
+++ AutoReservation.UI/ViewModels/AutoViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace AutoReservation.UI.ViewModels
+{
+    public enum AutoTyp
+    {
+        Standard,
+        Mittelklasse,
+        Luxus
+    }
+
+    public class AutoViewModel
+    {
+        public int Id;
+        public string Marke;
+        public AutoTyp Typ;
+        public double Tagestarif;
+        public double BasisTarif;
+    }
+
+    public class AutoMock
+    {
+        public static AutoViewModel Voiture = new AutoViewModel()
+        {
+            Id = 1,
+            Marke = "Fiat",
+            Typ = AutoTyp.Standard,
+            Tagestarif = 143.50
+        };
+    }
+}
