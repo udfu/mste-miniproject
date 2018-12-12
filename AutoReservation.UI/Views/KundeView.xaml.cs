e01cf915ecf45ddc97e21c1a36984010d271d756
Dani <daniel.bucher@windowslive.com>
2018-12-12 10:24:58 +0100

    added KundeView

---
+++ AutoReservation.UI/Views/KundeView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Service.Wcf;
+
+namespace AutoReservation.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for KundeView.xaml
+    /// </summary>
+    public partial class KundeView : Window
+    {
+        private AutoReservationService _reservationService;
+
+       public ObservableCollection<KundeDto> KundenDtos { get; set; }
+      
+        public KundeView()
+        {
+            InitializeComponent();
+
+            _reservationService = new AutoReservationService();
+            KundenDtos = new ObservableCollection<KundeDto>(_reservationService.ReadKundeDtos());
+            DataContext = this;
+
+
+        }
+    }
+}
