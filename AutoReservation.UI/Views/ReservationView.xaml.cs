977e81c0189f753dbd0b6f20a29d75e92c38110f
udfu <dario_delpiero10@hotmail.com>
2018-12-12 11:40:35 +0100

    Reservation UI v 0.0.1

---
+++ AutoReservation.UI/Views/ReservationView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MahApps.Metro.Controls;
+
+namespace AutoReservation.UI
+{
+    public partial class ReservationView : MetroWindow
+    {
+        public ReservationView()
+        {
+            InitializeComponent();
+            // DataContext = new AutoViewModel();
+            DataContext = AutoMock.Voiture;
+        }
+    }
+}
