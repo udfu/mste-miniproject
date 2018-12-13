fe3dc7ad2c7b63deac0d767bf7dc3ebac8e422dd
Dani <daniel.bucher@windowslive.com>
2018-12-13 11:07:13 +0100

    added delete functionality in KundeViewModel

--- AutoReservation.BusinessLayer/AutoManager.cs
+++ AutoReservation.BusinessLayer/AutoManager.cs
@@ -1,19 +1,32 @@
-﻿namespace AutoReservation.BusinessLayer
+﻿using System.Collections.Generic;
+using AutoReservation.Dal.Entities;
+
+namespace AutoReservation.BusinessLayer
 {
     public class AutoManager
         : ManagerBase
     {
-        // Example
-        //public List<Auto> List
-        //{
-        //    get
-        //    {
-        //        using (AutoReservationContext context = new AutoReservationContext())
-        //        {
-        //            return context.Autos.ToList();
-        //        }
-        //    }
-        //}
-
+//        public List<Auto> GetAutos()
+//        {
+//        }
+//
+//        public Auto GetAutoById(int id)
+//        {
+//        }
+//
+//        public void AddAuto( /*the needed parameters*/)
+//        {
+//
+//        }
+//
+//        public void UpdateAuto( int id/*the needed parameters*/)
+//        {
+//
+//        }
+//
+//        public void DeleteAuto(int id)
+//        {
+//
+//        }
     }
 }
--- AutoReservation.UI/ViewModels/KundeViewModel.cs
+++ AutoReservation.UI/ViewModels/KundeViewModel.cs
@@ -19,44 +19,67 @@ namespace AutoReservation.UI.ViewModels
         public KundeDto CurrentKundeDto { get; set; }
         public RelayCommand<KundeDto> DeleteCommand { get; set; }
 
-        private int _selectedValue;
-
-        public int SelectedValue
-        {
-            get { return _selectedValue; }
-            set
-            {
-                _selectedValue = value;
-                OnPropertyChanged(nameof(CurrentKundeDto));
-            }
-        }
+//        private int _selectedValue;
+//
+//        public int SelectedValue
+//        {
+//            get { return _selectedValue; }
+//            set
+//            {
+//                _selectedValue = value;
+//                OnPropertyChanged(nameof(CurrentKundeDto));
+//            }
+//        }
 
 
         public KundeViewModel()
         {
             KundenDtos = new ObservableCollection<KundeDto>(AppViewModel.Target.ReadKundeDtos());
-            CurrentKundeDto = KundenDtos[0];
+//            CurrentKundeDto = KundenDtos[0];
             DeleteCommand = new RelayCommand<KundeDto>(kunde => Remove());
-            SelectedValue = 0;
+//            SelectedValue = 0;
         }
 
         public void Remove()
         {
+//            KundenDtos.Remove(CurrentKundeDto);
+//           SelectedValue -= 1;
+//            OnPropertyChanged(nameof(SelectedValue)); //Problem, da mehrere Threads????
+//            OnPropertyChanged(nameof(KundenDtos));
+//            OnPropertyChanged(nameof(CurrentKundeDto));
+//
+
+//            int index = SelectedValue;
+//            AppViewModel.Target.deleteKunde(index + 1);
+//            KundenDtos.RemoveAt(index);
+
+            int index = CurrentKundeDto.Id;
+            AppViewModel.Target.deleteKunde(index);
             KundenDtos.Remove(CurrentKundeDto);
-            OnPropertyChanged(nameof(KundenDtos));
-            SelectedValue -= 1;
-            OnPropertyChanged(nameof(SelectedValue)); //Problem, da mehrere Threads????
+            CurrentKundeDto = null;
             OnPropertyChanged(nameof(CurrentKundeDto));
+
+
+            //            Console.Write(SelectedValue);
+            //            KundenDtos = new ObservableCollection<KundeDto>(AppViewModel.Target.ReadKundeDtos());
         }
 
 
         public void SelectedIndexChanged(object sender, System.EventArgs e)
         {
-            Console.Write(SelectedValue);
+//            Console.Write(SelectedValue);
 
-//            ListBox ourList = (ListBox) sender;
-            CurrentKundeDto = KundenDtos[SelectedValue];
-            OnPropertyChanged(nameof(CurrentKundeDto));
+            ListBox ourList = (ListBox) sender;
+
+            int index = ourList.SelectedIndex;
+
+            if (index >= 0)
+            {
+                CurrentKundeDto = KundenDtos[index];
+                OnPropertyChanged(nameof(CurrentKundeDto));
+            }
+
+           
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
